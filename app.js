c2b4bf0059ab788990ceab266ff20776566773dd
Ambuj Raj <thecoderguy@outlook.com>
2021-11-14 21:48:07 +0530

    boilerplate

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
+});
+
+// For Invalid URL
+app.use('*', (req, res) => {
+  res.status(400).json({
+    message: 'Invalid URL',
+  });
+});
+
+module.exports = app;
+
+// Notes -
+// Online users - https://stackoverflow.com/questions/47561708/count-number-of-online-users-with-nodejs-and-socket-io-without-duplicates
